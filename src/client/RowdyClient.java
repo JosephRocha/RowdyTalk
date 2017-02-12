3f34960e056fbcc28e7a17bbe23ab38ffa0e83ee
JosephRocha <RochaTrompeta>
2017-02-12 05:24:26 -0600

    Merged Server and Client Projects

--- src/client/RowdyClient.java
+++ src/client/RowdyClient.java
@@ -1,3 +1,4 @@
+package client;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- src/client/RowdyTalk.java
+++ src/client/RowdyTalk.java
@@ -1,3 +1,4 @@
+package client;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
